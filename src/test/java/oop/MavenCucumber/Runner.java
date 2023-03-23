c391d69c4fa16d0a9b135ec7758db3d7950f9160
calebmortensen <caleb.mortensen@live.com>
2023-03-23 09:41:36 -0700

    Pretty in Runner class

--- src/test/java/oop/MavenCucumber/Runner.java
+++ src/test/java/oop/MavenCucumber/Runner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"html:target/CucumberReport.html"})
+@CucumberOptions(plugin={"pretty","html:target/CucumberReport.html"})
 public class Runner {
 
 }
