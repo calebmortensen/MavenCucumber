da73fb83463b303147c56b501e5ffe16c730bd1d
calebmortensen <caleb.mortensen@live.com>
2023-05-29 13:03:02 -0700

    Read and Write files
    Example code in Rough package

---
+++ src/test/java/com/caleb/rough/FileReadRunner.java
@@ -0,0 +1,24 @@
+package com.caleb.rough;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileReadRunner {
+
+	public static void main(String[] args) throws IOException {
+		
+		//Path currentDirectory = Paths.get(".");
+		Path pathFileToRead = Paths.get(".//src//test//java//com//caleb//rough//data.txt");
+		
+		//List<String. lines = Files.readAllLines(pathFileToRead);
+		//System.out.println(lines);
+		
+		Files.lines(pathFileToRead)
+		.map(String::toLowerCase)
+		.filter(str -> str.contains("a"))
+		.forEach(System.out::println);
+	}
+
+}
