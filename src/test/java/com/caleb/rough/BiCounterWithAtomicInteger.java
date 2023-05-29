18658584cdb1d902880832104b2e9ec725bd122c
calebmortensen <caleb.mortensen@live.com>
2023-05-29 13:43:26 -0700

    Atomic Integer
    Makes Thread Safe

---
+++ src/test/java/com/caleb/rough/BiCounterWithAtomicInteger.java
@@ -0,0 +1,34 @@
+package com.caleb.rough;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BiCounterWithAtomicInteger {
+	
+	private AtomicInteger i = new AtomicInteger();
+	private AtomicInteger j = new AtomicInteger();
+		
+	
+	 public void incrementI() { 
+		
+		i.incrementAndGet(); //automatically makes THREAD SAFE
+		
+	}
+
+	public int getI() {
+		return i.get();
+	}
+
+	
+	
+	 public void incrementJ() { 
+		
+		j.incrementAndGet(); 
+		
+	}
+
+	public int getJ() {
+		return j.get();
+	}
+}
--- src/test/java/com/caleb/rough/ConcurrencyRunner.java
+++ src/test/java/com/caleb/rough/ConcurrencyRunner.java
@@ -4,10 +4,14 @@ public class ConcurrencyRunner {
 
 	public static void main(String[] args) {
 		
-		Counter counter = new Counter();
-		counter.increment();
+		//Counter counter = new Counter();
+		//counter.increment();
 		
-		System.out.println(counter.getI());
+		//System.out.println(counter.getI());
+		
+		BiCounterWithAtomicInteger biCounterWithAtomicInteger = new BiCounterWithAtomicInteger();
+		biCounterWithAtomicInteger.getI();
+		System.out.println(biCounterWithAtomicInteger.getI());
 
 	}
 
