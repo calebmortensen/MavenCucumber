eb72e5fceab8f3ee39a636f7d60910674655259a
calebmortensen <caleb.mortensen@live.com>
2023-05-31 09:24:44 -0700

    Static Import
    Example to reduce code

---
+++ src/test/java/com/caleb/rough/ImportRunner.java
@@ -0,0 +1,14 @@
+package com.caleb.rough;
+
+import static java.lang.System.out;
+
+public class ImportRunner {
+
+	public static void main(String[] args) {
+		
+		out.println("Static Imports");
+		
+
+	}
+
+}
