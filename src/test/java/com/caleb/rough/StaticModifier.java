e5aa60527763c8d1ae8ff178f1e519456278b3c3
calebmortensen <caleb.mortensen@live.com>
2023-06-02 10:27:05 -0700

    Update StaticModifier
    Removed instance call and made a class call for player method

--- src/test/java/com/caleb/rough/StaticModifier.java
+++ src/test/java/com/caleb/rough/StaticModifier.java
@@ -1,7 +1,7 @@
 package com.caleb.rough;
 
 class Player {
-	private String name;
+	private String name;            //name is an instance variable or member variable
 	private static int count = 0;   //without the ****'static'**** modifier, count would not increment
 	                                //count variable is shared between multiple instances of the player objects
 
@@ -12,23 +12,29 @@ class Player {
 		count ++;
 	}
 
-	public int getCount() {
+	public static int getCount() {        //added static
+		//System.out.println(name);       //won't work - can't make a static reference to non-static field
 		return count;
 	}
 
-	
 	/* public void setCount(int count) { Player.count = count; } */
 	 
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 }
 
 public class StaticModifier {
 
 	public static void main(String[] args) {
 		Player player1 = new Player("Ronaldo");
-		System.out.println(player1.getCount());
+		System.out.println(Player.getCount());     //getCount belongs to class. Removed instance call to player1.getCount
 		Player player2 = new Player("Federer");
-		System.out.println(player2.getCount());
+		System.out.println(Player.getCount());
 
 	}
 
